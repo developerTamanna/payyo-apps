098ba013b6cc87157f1d46123b218a8ceb233e6c
developerTamanna <astamanna57@gmail.com>
2025-02-27 15:28:07 +0600

    pay bill history ad

--- script/payBill.js
+++ script/payBill.js
@@ -8,6 +8,10 @@ document.getElementById('pay-bill-btn').addEventListener('click', function(event
     const pinNumber = getInputValueByID('py-bill-pin-number')
     // console.log(pinNumber)
     const mainBalance =getInnerTextByID('main-balance-add')
+    
+  const selectedBank = document.getElementById('all-bank').value;
+  console.log(selectedBank)
+
 
     if(accountNumber.length ===11) {
         // console.log('oky');
@@ -15,6 +19,18 @@ document.getElementById('pay-bill-btn').addEventListener('click', function(event
            
            const sum = mainBalance - amount;
            setInnerTextByIDandValue('main-balance-add', sum)
+
+        const container = document.getElementById('transaction-container');
+        const div = document.createElement('div');
+        const date = new Date();
+        div.innerHTML = `
+        <h1>bill paid from ${selectedBank}</h1>
+        <h3>${amount} </h3>
+        <p>account number: ${accountNumber}</p>
+         <small> ${date.toISOString()}</small>
+        `
+        container.appendChild(div);
+
         }
         else{
 
